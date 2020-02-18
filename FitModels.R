32c91d0318ebcd3cce19ae89ec71bf5e1a5d8368
David Buch <davidbuch42@gmail.com>
2020-02-18 01:15:26 -0500

    Uploading Presentation

--- FitModels.R
+++ FitModels.R
@@ -1,13 +1,22 @@
 library(rjags)
 
+load(file = "data_1999.rdata")
+
 ## LOAD AND CLEAN DATA
-dat <- dat99[sample(nrow(dat99), size = 0.1*nrow(dat99)),]
+dat <- dat99[sample(nrow(dat99), size = 0.2*nrow(dat99)),]
 
 N <- nrow(dat)
-X <- dat[,c("G18", "G19", "MEMGREEK")]
-names(X) <- c("Dad.School", "Mom.School", "Greek")
+X <- dat[,c("G18", "G19", "MEMGREEK", "G10", "SEX", "A3")]
+names(X) <- c("Dad.School", "Mom.School", "Greek", "HS.Drink", "Sex", "Grade")
+# HS_Drink 1 -- never, 2,3 -- Infrequent, 4,5,6,7 -- Frequent
+# Year 1 fresh, 2 soph, 3, 4, 5 (fifth year), 6 grad student
+# Race
+# Religion
 X$Dad.School <- factor(X$Dad.School, levels = c(1,2,3,4), labels = c(0,0,0,1))
 X$Mom.School <- factor(X$Mom.School, levels = c(1,2,3,4), labels = c(0,0,0,1))
+X$HS.Drink <- factor(X$HS.Drink, levels = c(1,2,3,4,5,6,7), labels = c("Never","Infreq","Infreq","Freq","Freq","Freq","Freq"))
+X$Sex <- factor(X$Sex, levels = c(0,1), labels = c("Male", "Female"))
+X$Grade <- factor(X$Grade, levels = c(1,2,3,4,5,6), labels = c("Fresh", "Soph", "Junior", "Senior", "Fifth", "Grad"))
 X$First.Gen <- factor(as.numeric((X$Dad.School == 0)&(X$Mom.School == 0)))
 X$Greek <- factor(X$Greek)
 summary(X)
@@ -27,7 +36,7 @@ GPA <- YYX[,M+1]
 X <- YYX[,(M + 2):ncol(YYX)]
 
 ## Get Final Model Matrix
-X <- model.matrix(~First.Gen*Greek, data = X)
+X <- model.matrix(~First.Gen*Greek + HS.Drink + Sex + Grade, data = X)
 
 
 
@@ -44,7 +53,7 @@ X <- model.matrix(~First.Gen*Greek, data = X)
 drink_prob_model <- jags.model(textConnection(drink_prob_model_string), 
                     data = list(X=X, Y=drink_problems, N=nrow(drink_problems), p=ncol(X), M=ncol(drink_problems), K=2))
 
-samp <- coda.samples(drink_prob_model, variable.names=c("beta", "xi"), n.iter=300)
+samp <- coda.samples(drink_prob_model, variable.names=c("beta", "xi"), n.iter=1000)
 summary(samp)
 
 ## --- Academic Performance Model ---
@@ -60,7 +69,7 @@ g <- n # For Zellner g-prior
 
 
 ## GIBBS SAMPLING ROUTINE
-S <- 100 # number of simulations
+S <- 1000 # number of simulations
 
 ## Data Storage
 # Don't need to store samples of Z
@@ -68,12 +77,12 @@ BETA <- NULL
 
 ## Starting Values for Gibbs Sampling
 Z <- 6*(rank(Y) - mean(rank(Y)))/n
-beta <- rep(0,3) # First.Gen, Greek, and First.Gen:Greek
+beta <- rep(0,11) # First.Gen, Greek, and First.Gen:Greek
 
 ## Sample
 for(s in 1:S){
   # Sample beta
-  w <- rnorm(3)
+  w <- rnorm(11)
   w <- t(V)%*%w
   w <- g*w/(1 + g)
   beta <- w + (g/(1 + g))*XtXi%*%t(X)%*%Z
@@ -94,4 +103,22 @@ for(s in 1:S){
   
   BETA <- rbind(BETA, t(beta))
 }
+
+
 colMeans(BETA) # Mean Coefficients (Positive indicates improved GPA)
+par(mfrow = c(1,3))
+for(i in 1:3){plot(density(BETA[,i]), lwd = 2, col = "blue", main = paste("Effect on GPA: ", c("First Gen", "Greek", "Inter"))[i], xlab = NA); abline(v = 0, col = "red")}
+
+## But these Traceplots Suggest Burnin Should be Dropped
+plot(BETA[,1])
+plot(BETA[,2])
+plot(BETA[,3])
+
+## So Trim Beta
+BETA_trim <- BETA[300:1000,]
+for(i in 1:3){plot(density(BETA_trim[,i]), lwd = 2, col = "blue", main = paste("Effect on GPA: ", c("First Gen", "Greek", "Inter"))[i], xlab = NA); abline(v = 0, col = "red")}
+
+
+
+
+
